536fa91b42a039f65e89e027ffc9073c7baa4c3c
aljml-cubes <mohammed.aljamal@cubesplatform.com>
2025-09-23 10:38:39 +0300

    init: install tailwind and finish the steup

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <Main msg="Tailwind & CSS" />
+  <h1 class="text-3xl font-bold text-blue-600">{{ "Tailwind & CSS" }}</h1>
+</template>
+
+<script setup lang="ts">
+import Main from "./components/main.vue";
+</script>
